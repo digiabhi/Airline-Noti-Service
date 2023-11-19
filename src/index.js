d12456a56e0604cd25ac69e45cabe4a23f7e5673
Abhishek Singh <singhabhishek111213@gmail.com>
2023-11-19 19:44:49 +0530

    Completed consumption & sending mail

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-const express = require('express')
+const express = require('express');
+const amqplib  = require("amqplib");
+const { EmailService } = require('./services')
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue", async (data) => {
+            console.log(`${Buffer.from(data.content)}`);
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            // const object = JSON.parse(Buffer.from(data).toString());
+            await EmailService.sendEmail("airlinenoti@gmail.com", object.recepientEmail, object.subject, object.text);
+            channel.ack(data);
+        })
+    } catch(error) {
+        
+    }
+}
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+
+const mailsender = require('./config/email-config')
 const app = express();
-const mailSender = require("./config/email-config");
-const {error} = require("winston");
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, ()=>{
-    console.log(`Successfully Started Server on PORT: ${ServerConfig.PORT}`)
-
+app.listen(ServerConfig.PORT, async () => {
+    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    await connectQueue();
+    console.log("queue is up")
 });
